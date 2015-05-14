d383d0eced1e908eac338282e2003153406781bf
Kristján Oddsson <koddsson@gmail.com>
2015-05-14 15:01:15 +0000

    Update the UI

--- booze.js
+++ booze.js
@@ -28,9 +28,24 @@ var manager;
 
   var display_data = function(data, shadow) {
     data.forEach(function(item) {
-      let row = document.createElement('a');
-      row.innerHTML = item.title;
-      row.setAttribute('href', item.link);
+      let row = document.createElement('div');
+      
+      let title = document.createElement('a');
+      title.setAttribute('href', item.link);
+      title.innerHTML = item.title;
+      title.classList.add('title');
+     
+      let price = document.createElement('span');
+      price.innerHTML = item.price;
+      price.classList.add('price');
+      
+      let weight = document.createElement('span'); 
+      weight.innerHTML = item.weight;
+      weight.classList.add('weight');
+     
+      row.appendChild(title); 
+      row.appendChild(price);
+      row.appendChild(weight);
       row.classList.add('row');
       shadow.querySelector('.data').appendChild(row);
     });
@@ -57,6 +72,7 @@ var manager;
         return response.json();
       }).then(function(json) {
         manager = new BoozeManager(json);
+        this.shadow.querySelector('.data').classList.remove('is-loading');
         display_data(json, this.shadow);
       }.bind(this)).catch(function(ex) {
         console.log(ex);
